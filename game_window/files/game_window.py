a5a426fa2d9d08a368a5c533f9115eaedb8710b9
Sanechek <bogdanovalexandr1996@gmail.com>
2024-03-24 21:13:47 +0300

    24.03 Game engine on

--- game_window/files/game_window.py
+++ game_window/files/game_window.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # Инициализация Pygame
 pygame.init()
@@ -13,6 +14,12 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Catch The Cat !')
 
 
+class Game():
+    def __init__(self):
+        self.score = 0
+        self.lifes = 3
+
+
 # Класс для статических изображений наследуется от основного окна
 class StaticImage(pygame.sprite.Sprite):
     def __init__(self, image_path, x, y, sizex, sizey):
@@ -24,33 +31,79 @@ class StaticImage(pygame.sprite.Sprite):
         self.rect.y = y
 
 
+class Cat(pygame.sprite.Sprite):
+    def __init__(self, image_path, track, catch_pos):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.track = track
+        self.rect = self.image.get_rect()
+        self.pos = 0
+        self.catch_pos = catch_pos
+        self.rect.x, self.rect.y = track[self.pos]
+
+    def next_pos(self):
+        if self.pos + 1 < len(self.track):
+            self.pos += 1
+            self.rect.x, self.rect.y = self.track[self.pos]
+        else:
+            if hero.pos == self.catch_pos:
+                game.score += 1
+            else:
+                game.lifes -= 1
+            dynamic_group.remove(self)
+
+
 class Hero(StaticImage):
     def __init__(self, image_path, x, y, sizex, sizey):
         super().__init__(image_path, x, y, sizex, sizey)
         self.pos = 1
 
-    def change_pos(self):
-        pass
+    def change_pos(self, event):
+        size = self.image.get_size()
+        if event.key == pygame.K_UP:
+            self.image = pygame.image.load('../dynamic/1.png').convert_alpha()
+            self.pos = 1
+        elif event.key == pygame.K_DOWN:
+            self.image = pygame.image.load('../dynamic/3.png').convert_alpha()
+            self.pos = 3
+        elif event.key == pygame.K_LEFT:
+            self.image = pygame.image.load('../dynamic/2.png').convert_alpha()
+            self.pos = 2
+        elif event.key == pygame.K_RIGHT:
+            self.image = pygame.image.load('../dynamic/4.png').convert_alpha()
+            self.pos = 4
+        self.image = pygame.transform.scale(self.image, size)
 
 
 # Создание группы спрайтов для статичных обьектов
 static_group = pygame.sprite.Group()
-# Размер кнопок
+# Создание группы спрайтов для динамичных обьектов
+dynamic_group = pygame.sprite.Group()
+game = Game()
+# Размер статичных спрайтов
 SIZE_X = 300
 SIZE_Y = 150
 
-# Загрузка изображений для треков
-
+# Загрузка изображений для треков и создание наборов точек для перемещений
 left_rail_up = StaticImage('../static/1.png', 0,
                            WINDOW_HEIGHT * 0.2, SIZE_X, SIZE_Y)
+left_rail_up_tr = []
+
 right_rail_up = StaticImage('../static/2.png', WINDOW_WIDTH - SIZE_X,
                             WINDOW_HEIGHT * 0.2, SIZE_X, SIZE_Y)
+right_rail_up_tr = []
+
 left_rail_low = StaticImage('../static/3.png', 0,
                             WINDOW_HEIGHT - SIZE_Y, SIZE_X, SIZE_Y)
+left_rail_low_tr = [(x, WINDOW_HEIGHT - SIZE_Y) for x in range(0, 300, 50)]
+
 right_rail_low = StaticImage('../static/3.png', WINDOW_WIDTH - SIZE_X,
                              WINDOW_HEIGHT - SIZE_Y, SIZE_X, SIZE_Y)
 
-# Добавление кнопок в группу спрайтов
+right_rail_low_tr = []
+
+# Добавление направляющих в группу спрайтов
 for elem in (left_rail_up, right_rail_up, left_rail_low, right_rail_low):
     static_group.add(elem)
 
@@ -58,22 +111,25 @@ for elem in (left_rail_up, right_rail_up, left_rail_low, right_rail_low):
 HERO_HEIGHT = 400
 HERO_WIDTH = 200
 
-# Загрузка изображения для героя
-hero = Hero('../dynamic/base.png', (WINDOW_WIDTH - HERO_WIDTH) // 2,
+# Загрузка изображений
+hero = Hero('../dynamic/1.png', (WINDOW_WIDTH - HERO_WIDTH) // 2,
             WINDOW_HEIGHT - HERO_HEIGHT, HERO_WIDTH, HERO_HEIGHT)
+
+cat = Cat('../dynamic/cat.png', left_rail_low_tr, 2)
+dynamic_group.add(cat)
 
 background = pygame.image.load(f"../static/bg.jpg")
 background = pygame.transform.scale(background, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
+# Установка таймера анимации и генерации
 
-# Функция для изменения цвета фона
-def change_background_color():
-    current_time = pygame.time.get_ticks()  # Получение текущего времени в миллисекундах
-    index = (current_time // 500) % 21  # Изменение номера картинки
-    background = pygame.image.load(f"../static/anim_background/{index}.png")
-    background = pygame.transform.scale(background, (WINDOW_WIDTH, WINDOW_HEIGHT))
-    return background
+animation_timer = pygame.time.get_ticks()
+generation_timer = pygame.time.get_ticks()
+# Установка шрифта
+font = pygame.font.SysFont(None, 48)
 
+# Начальное значение счета
+score = 0
 
 # Основной цикл программы
 running = True
@@ -83,17 +139,26 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = event.pos
-
+        elif event.type == pygame.KEYDOWN:
+            hero.change_pos(event)
+    if pygame.time.get_ticks() - generation_timer > (random.choice[1000, 2000, 3000, 4000]):
+        pass # Не забудь написать код
+    if pygame.time.get_ticks() - animation_timer > 1000:
+        for elem in dynamic_group:
+            elem.next_pos()
+        animation_timer = pygame.time.get_ticks()
     # Очистка экрана
     screen.blit(background, (0, 0))
     screen.blit(hero.image, (hero.rect.x, hero.rect.y))
-
     # Отображение треков
     static_group.draw(screen)
-
+    # Отображение двигающихся обьектов
+    dynamic_group.draw(screen)
+    # Отрисовка счета
+    score_text = font.render(f'Score: {game.score}', True, 'BLACK')
+    screen.blit(score_text, (10, 10))
     # Обновление экрана
     pygame.display.flip()
-
 # Завершение работы Pygame
 pygame.quit()
 sys.exit()
